0259c61f1679c9ee64927a8ee988e24587c4db65
Aman Thakur <builtbyaman@gmail.com>
2022-04-13 12:20:00 +0530

    added express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-const http = require('http');
+const app = require('./server');
 
-const server = http.createServer(async (req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    console.log('server saying hello');
-    res.end();
-  }
-});
-
-server.listen(3001, () => {
-  console.log('server on http://localhost:3001');
+app.listen(3001, () => {
+  console.log(`server started on http://localhost:3001`);
 });
