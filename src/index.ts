4e98ec05cbf8d81834f2068b722ec56718f8fe62
Aman Thakur <builtbyaman@gmail.com>
2022-04-20 11:31:00 +0530

    authentication

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import app from './server';
+import dotenv from 'dotenv';
+dotenv.config();
 
 app.listen(3001, () => {
   console.log(`server started on http://localhost:3001`);
---
+++ src/modules/auth.ts
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+
+export const comparePasswords = (password: string, hashedPassword: string) => {
+  return bcrypt.compare(password, hashedPassword)
+}
+
+export const hashPassword = (password: string) => {
+  return bcrypt.hash(password, 5)
+}
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) return res.status(401).json({ message: 'Not authorized' });
+
+  const [, token] = bearer.split(' ');
+  if (!token) return res.status(401).json({ message: 'Not authorized' });
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e);
+    return res.status(401).json({ message: 'Not valid token' });
+  }
+};
